9b223b6c7dd9d8c6f47efa6be6fce59dafc384ff
Yamori-developer <homeiot-dev@kddi.com>
2018-07-11 16:25:30 +0900

    add: テストケースを書いたよ

--- flathand/flathandTests/flathandTests.swift
+++ flathand/flathandTests/flathandTests.swift
@@ -22,8 +22,7 @@ class flathandTests: XCTestCase {
     }
     
     func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        XCTAssertEqual(0, 1)
     }
     
     func testPerformanceExample() {
