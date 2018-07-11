9f9c0636c51527dfbbd930e3aad3568f8efbd172
Yamori-developer <homeiot-dev@kddi.com>
2018-07-11 16:22:34 +0900

    add: プロジェクト作成

---
+++ flathand/flathand/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  flathand
+//
+//  Created by yamori-developer on 2018/07/11.
+//  Copyright © 2018年 yamori-developer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
